b8d4c3ab72e29e113036d73ec483617c3975af13
Satyajit Talukder <dorhubs@gmail.com>
2023-06-08 11:52:10 +0600

    fix(time): it fixes the issue of alphanumeric input
    This fix fixes the issue of alphanumeric problem. Before the fix it was registering alphanumeric values after the fix it only registers the numeric values.
    
    Resolves: #36

--- includes/modules/sales-pop/assets/build/settings.asset.php
+++ includes/modules/sales-pop/assets/build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => 'ad545ff84f199bddf07b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => '3ac20bbf25bde76858a7');
