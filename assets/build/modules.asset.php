307bca6a9d6ce50991c93be48d71470b9026c8c2
Ariful Islam <arif13414@gmail.com>
2023-06-18 11:06:42 +0600

    Show setting button when enable module

--- assets/build/modules.asset.php
+++ assets/build/modules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '4ade9a60618d9b197401');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '979e88c7f2db861a91ee');
