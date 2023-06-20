3f749e14d2d58508f39744760b3676721d724ab7
Satyajit Talukder <dorhubs@gmail.com>
2023-06-20 11:52:17 +0600

    style(discount-banner): design update
    The design has been updated form the default design to newly implemented desing. That looks much more robust.
    
    Resolves: #92

--- includes/modules/progressive-discount-banner/assets/build/settings.asset.php
+++ includes/modules/progressive-discount-banner/assets/build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => '453baa5954be1195ca8f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => 'ab89a831ba54dec7eb4d');
--- includes/modules/progressive-discount-banner/templates/bar.php
+++ includes/modules/progressive-discount-banner/templates/bar.php
@@ -17,13 +17,14 @@ $banner_icon = sgsb_pd_banner_get_banner_icon( $settings );
 ?>
 <div class="sgsb-pd-banner-bar-wrapper">
 	<div class="sgsb-pd-banner-bar">
-		<p>
+		<div class="sgsb-pd-banner-bar-icon">
 			<?php
 			if ( $banner_icon ) {
 				echo $banner_icon; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			}
 			?>
-			<span>
+		</div>
+			<span class="sgsb-pd-banner-text">
 				<?php
 				/**
 				 * Banner text filter.
@@ -33,6 +34,8 @@ $banner_icon = sgsb_pd_banner_get_banner_icon( $settings );
 				echo wp_kses_post( apply_filters( 'sales_boster_pd_banner_text', $banner_text ) );
 				?>
 			</span>
-		</p>
+			
+				<img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) . '../assets/images/sgsb-pd-banner-bar-remove.svg' ); ?>" alt="remove">
+			
 	</div>
 </div>
