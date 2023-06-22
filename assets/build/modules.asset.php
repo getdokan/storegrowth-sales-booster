063aceb7c7b7f5cbdfb0d41f95c99d1b3d78d8d9
hz-tyfoon <md.tifun@gmail.com>
2023-06-22 21:43:01 +0600

    feat: updated the logo & favicon
    Resolves #97

--- assets/build/modules.asset.php
+++ assets/build/modules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '4b37f7a4ac7473f04a40');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '75ca0deb7bf6ede92f47');
