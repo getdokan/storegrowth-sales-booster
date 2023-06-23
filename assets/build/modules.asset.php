52d5401c3fdfeef197aaca40d0f9105a0835227d
Ariful Islam <arif13414@gmail.com>
2023-06-23 21:44:29 +0600

    Update ui of settings sidebar

--- assets/build/modules.asset.php
+++ assets/build/modules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '75ca0deb7bf6ede92f47');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '3ed55406242d24f3d28c');
