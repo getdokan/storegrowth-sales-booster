94c7a49131cc9512055961072c7cad57b46dc757
hz-tyfoon <md.tifun@gmail.com>
2023-06-25 14:52:19 +0600

    feat: the cross icon made more easy to click

--- assets/build/modules.asset.php
+++ assets/build/modules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '07c55e784da935e5e1d1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'e91e060b8fbe57182cbd');
