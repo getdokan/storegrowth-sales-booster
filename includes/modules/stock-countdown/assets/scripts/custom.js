309de1c1bbdf68f9a79f8048c5922c0a6787446f
Satyajit Talukder <dorhubs@gmail.com>
2023-07-10 11:00:33 +0600

    fix(stock-countdown): merge confilict fix

--- includes/modules/stock-countdown/assets/scripts/custom.js
+++ includes/modules/stock-countdown/assets/scripts/custom.js
@@ -1,41 +1,33 @@
-"use strict";
+'use strict';
 
-jQuery(document).ready(function ($) {
+jQuery(document).ready( function($) {
   // Stock progress bar.
   $(".jqmeter-container").each(function () {
-      var goal = $(this).parent().attr("total-stock");
-      var raised = $(this).parent().attr("total-sale");
-      var height = $(this).parent().data("height") + "px";
-      var bgColor = $(this).parent().data("bgcolor");
-      var barColor = $(this).parent().data("fgcolor");
-      $(this).jQMeter({
-          goal: goal,
-          raised: raised,
-          meterOrientation: 'horizontal',
-          width: '100%',
-          height: height,
-          bgColor: bgColor,
-          barColor: barColor,
-          displayTotal: false,
-      });
+    var goal = $(this).parent().attr('total-stock');
+    var raised = $(this).parent().attr('total-sale');
+    var height = $(this).parent().data('height') + 'px';
+    var bgColor = $(this).parent().data('bgcolor');
+    var barColor = $(this).parent().data('fgcolor');
+    $(this).jQMeter({
+        goal: goal,
+        raised: raised,
+        meterOrientation: 'horizontal',
+        width: '100%',
+        height: height,
+        bgColor: bgColor,
+        barColor: barColor,
+        displayTotal: false,
+    });
+});
+
+  $('.sgsb-stock-counter-items').each(function() {
+    $(this).countdown( $(this).data('end-date') )
+        .on('update.countdown', function(event) {
+        $(this).find('strong.sgsb-stock-counter-item-days').html(event.strftime('%D'));
+        $(this).find('strong.sgsb-stock-counter-item-hours').html(event.strftime('%H'));
+        $(this).find('strong.sgsb-stock-counter-item-minutes').html(event.strftime('%M'));
+        $(this).find('strong.sgsb-stock-counter-item-seconds').html(event.strftime('%S'));
+        });
   });
 
-  $(".sgsb-stock-counter-items").each(function () {
-    $(this)
-      .countdown($(this).data("end-date"))
-      .on("update.countdown", function (event) {
-        $(this)
-          .find("strong.sgsb-stock-counter-item-days")
-          .html(event.strftime("%D"));
-        $(this)
-          .find("strong.sgsb-stock-counter-item-hours")
-          .html(event.strftime("%H"));
-        $(this)
-          .find("strong.sgsb-stock-counter-item-minutes")
-          .html(event.strftime("%M"));
-        $(this)
-          .find("strong.sgsb-stock-counter-item-seconds")
-          .html(event.strftime("%S"));
-      });
-  });
-});
+} );
