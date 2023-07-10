b4c566fec30f6f50ef08f529afa46ae206cdb2dd
Satyajit Talukder <dorhubs@gmail.com>
2023-07-10 10:15:04 +0600

    fix(stock-countdown): fixed progress bar data input.
    Description:
    Before the fix the progress bar was only taking the indivisual data of the progress bar was not being applied. Same kind of data was being applied everywhere. In this fix the progress bar is taking its indivisual data field and problem has been fixed.
    
    Resolves: #146

--- includes/modules/stock-countdown/assets/scripts/custom.js
+++ includes/modules/stock-countdown/assets/scripts/custom.js
@@ -1,26 +1,41 @@
-'use strict';
+"use strict";
 
-jQuery(document).ready( function($) {
+jQuery(document).ready(function ($) {
   // Stock progress bar.
-  $('.jqmeter-container').jQMeter({
-    goal: $('.wpbsc_total_sale').attr('total-stock'),
-    raised: $('.wpbsc_total_sale').attr('total-sale'),
-    meterOrientation: 'horizontal',
-    width: '100%',
-    height: $('.wpbsc_total_sale').data('height') + "px",
-    bgColor: $('.wpbsc_total_sale').data('bgcolor'),
-    barColor: $('.wpbsc_total_sale').attr('fgcolor'),
-    displayTotal: false,
+  $(".jqmeter-container").each(function () {
+      var goal = $(this).parent().attr("total-stock");
+      var raised = $(this).parent().attr("total-sale");
+      var height = $(this).parent().data("height") + "px";
+      var bgColor = $(this).parent().data("bgcolor");
+      var barColor = $(this).parent().data("fgcolor");
+      $(this).jQMeter({
+          goal: goal,
+          raised: raised,
+          meterOrientation: 'horizontal',
+          width: '100%',
+          height: height,
+          bgColor: bgColor,
+          barColor: barColor,
+          displayTotal: false,
+      });
   });
 
-  $('.sgsb-stock-counter-items').each(function() {
-	$(this).countdown( $(this).data('end-date') )
-		.on('update.countdown', function(event) {
-		$(this).find('strong.sgsb-stock-counter-item-days').html(event.strftime('%D'));
-		$(this).find('strong.sgsb-stock-counter-item-hours').html(event.strftime('%H'));
-		$(this).find('strong.sgsb-stock-counter-item-minutes').html(event.strftime('%M'));
-		$(this).find('strong.sgsb-stock-counter-item-seconds').html(event.strftime('%S'));
-		});
+  $(".sgsb-stock-counter-items").each(function () {
+    $(this)
+      .countdown($(this).data("end-date"))
+      .on("update.countdown", function (event) {
+        $(this)
+          .find("strong.sgsb-stock-counter-item-days")
+          .html(event.strftime("%D"));
+        $(this)
+          .find("strong.sgsb-stock-counter-item-hours")
+          .html(event.strftime("%H"));
+        $(this)
+          .find("strong.sgsb-stock-counter-item-minutes")
+          .html(event.strftime("%M"));
+        $(this)
+          .find("strong.sgsb-stock-counter-item-seconds")
+          .html(event.strftime("%S"));
+      });
   });
-
-} );
+});
