fda2f86e6f887180de708016e6423484ff2a46e8
Satyajit Talukder <dorhubs@gmail.com>
2023-07-17 11:15:05 +0600

    fix(upsell-order-bump): color code updated of teaser message

--- assets/build/modules.asset.php
+++ assets/build/modules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'c62dbc5e74fbe1421570');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '22b9e758118c7600edb4');
