c5a2208cf1f6510fa4af6b6125ec782ee7f25355
hz-tyfoon <md.tifun@gmail.com>
2023-07-19 11:41:09 +0600

    feat: (stock-bar): refactored/cleaned and removed unnecessary codes

--- includes/modules/stock-bar/templates/simple-stock-status.php
+++ includes/modules/stock-bar/templates/simple-stock-status.php
@@ -5,7 +5,14 @@
  * @package SBFW
  */
 
-if ( ! sgsb_stock_bar_is_product_discountable( $product->get_id() ) && ! $product->managing_stock() ) {
+if ( ! $product->managing_stock() ) {
+	return;
+}
+
+$settings                         = get_option( 'sgsb_stock_bar_settings' );
+$enable_stock_bar_in_product_page = sgsb_find_option_setting( $settings, 'product_page_stock_bar_enable', true );
+
+if ( is_product() && ! $enable_stock_bar_in_product_page ) {
 	return;
 }
 
@@ -13,20 +20,45 @@ $total_sales = intval( $product->get_total_sales() );
 $stock       = intval( $product->get_stock_quantity() );
 $total_stock = $stock + $total_sales;
 
-$settings                 = get_option( 'sgsb_stock_bar_settings' );
-$product_bar_enable       = sgsb_find_option_setting( $settings, 'product_page_stock_bar_enable', true );
-$product_countdown_enable = sgsb_find_option_setting( $settings, 'product_page_countdown_enable', true );
-
-
-$hide_template_in_product = is_product() && ( ! $product->managing_stock() || ! $product_bar_enable ) && ! $product_countdown_enable;
-
-if ( $hide_template_in_product ) {
-	return false;
-}
-
+$bar_height          = sgsb_find_option_setting( $settings, 'stockbar_height', '5' );
+$bg_color            = sgsb_find_option_setting( $settings, 'stockbar_bg_color', '#444444' );
+$fg_color            = sgsb_find_option_setting( $settings, 'stockbar_fg_color', '#c3d168' );
+$sd_format           = sgsb_find_option_setting( $settings, 'stock_display_format', 'above' );
+$total_sell_text     = sgsb_find_option_setting( $settings, 'total_sell_count_text', 'Total Sold' );
+$available_item_text = sgsb_find_option_setting( $settings, 'available_item_count_text', 'Available Item' );
 ?>
-<div class="sgsb-stock-counter-and-bar">
-	<?php
-		require __DIR__ . '/stock-bar.php';
-	?>
+
+<div class="sgsb-stock-bar">
+	<div
+		class="sgsb-stock-stock-bar wpbsc_total_sale sgsb-stock-stock-bar-format-<?php echo esc_attr( $sd_format ); ?>"
+		total-sale="<?php echo esc_attr( $total_sales ); ?>"
+		total-stock="<?php echo esc_attr( $total_stock ); ?>"
+		data-height="<?php echo esc_attr( $bar_height ); ?>"
+		data-bgcolor="<?php echo esc_attr( $bg_color ); ?>"
+		data-fgcolor="<?php echo esc_attr( $fg_color ); ?>"
+	>
+		<?php if ( 'above' === $sd_format ) : ?>
+		<div class="sgsb-stock-stockbar-status">
+			<span class="total-sold">
+				<?php echo esc_html( sprintf( '%1$s:', $total_sell_text ) ); ?>
+				<span class="total-sale">
+					<?php echo esc_html( sprintf( '%1$s', $total_sales ) ); ?>
+				</span>
+			</span>
+			<span class="instock">
+				<?php echo esc_html( sprintf( '%1$s:', $available_item_text ) ); ?>
+				<span class="total-sale">
+					<?php echo esc_html( sprintf( '%1$s', $stock ) ); ?>
+				</span>
+			</span>
+		</div>
+		<?php endif; ?>
+		<div class="jqmeter-container"></div>
+		<?php
+		if ( 'below' === $sd_format ) :
+				/* translators: %s: Left items in stock */
+				do_action( 'sgsb_stock_bar_stock_below' );
+			endif;
+		?>
+	</div>
 </div>
