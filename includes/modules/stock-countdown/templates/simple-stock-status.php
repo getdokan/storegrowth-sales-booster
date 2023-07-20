a34291dda144713afb8db1a7ee27a9e7e986d0a1
hz-tyfoon <md.tifun@gmail.com>
2023-07-20 17:44:50 +0600

    feat: (stock-countdown): removed stock-bar related codes in php end

--- includes/modules/stock-countdown/templates/simple-stock-status.php
+++ includes/modules/stock-countdown/templates/simple-stock-status.php
@@ -5,30 +5,54 @@
  * @package SBFW
  */
 
-if ( ! sgsb_stock_cd_is_product_discountable( $product->get_id() ) && ! $product->managing_stock() ) {
+if ( ! sgsb_stock_cd_is_product_discountable( $product->get_id() ) ) {
 	return;
 }
 
-$total_sales = intval( $product->get_total_sales() );
-$stock       = intval( $product->get_stock_quantity() );
-$total_stock = $stock + $total_sales;
+$settings                         = get_option( 'sgsb_stock_countdown_settings' );
+$enable_countdown_in_product_page = sgsb_find_option_setting( $settings, 'product_page_countdown_enable', true );
 
-$settings                 = get_option( 'sgsb_stock_countdown_settings' );
-$product_bar_enable       = sgsb_find_option_setting( $settings, 'product_page_progress_bar_enable', true );
-$product_countdown_enable = sgsb_find_option_setting( $settings, 'product_page_countdown_enable', true );
-
-
-$hide_template_in_product = is_product() && ( ! $product->managing_stock() || ! $product_bar_enable ) && ! $product_countdown_enable;
-
-if ( $hide_template_in_product ) {
-	return false;
+if ( is_product() && ! $enable_countdown_in_product_page ) {
+	return;
 }
+
+$start_date = get_post_meta( $product->get_id(), '_sgsb_stock_countdown_discount_start', true );
+$end_date   = get_post_meta( $product->get_id(), '_sgsb_stock_countdown_discount_end', true );
+
+$discount_amount = get_post_meta( $product->get_id(), '_sgsb_stock_countdown_discount_amount', true );
+
+$heading_text = sgsb_find_option_setting( $settings, 'countdown_heading', 'Last chance! [discount]% OFF' );
+$heading      = str_replace( '[discount]', $discount_amount, $heading_text );
 
 ?>
 <div class="sgsb-stock-counter-and-bar">
-	<?php
-		require __DIR__ . '/stock-counter.php';
 
-		require __DIR__ . '/stock-bar.php';
-	?>
+	<div class="sgsb-stock-counter-wrapper">
+		<?php if ( $heading_text ) : ?>
+		<p class="sgsb-stock-counter-heading"><?php echo wp_kses_post( $heading ); ?></p>
+		<?php endif; ?>
+
+		<div class="sgsb-stock-counter-items" data-end-date="<?php echo esc_attr( $end_date ); ?>">
+			<div class="sgsb-stock-counter-item">
+				<strong class="sgsb-stock-counter-item-days">00</strong>
+				<span>Days</span>
+			</div>
+			<span class="sgsb-colon">:</span>
+			<div class="sgsb-stock-counter-item">
+				<strong class="sgsb-stock-counter-item-hours">00</strong>
+				<span>Hours</span>
+			</div>
+			<span class="sgsb-colon">:</span>
+			<div class="sgsb-stock-counter-item">
+				<strong class="sgsb-stock-counter-item-minutes">00</strong>
+				<span>Min</span>
+			</div>
+			<span class="sgsb-colon">:</span>
+			<div class="sgsb-stock-counter-item">
+				<strong class="sgsb-stock-counter-item-seconds">00</strong>
+				<span>Sec</span>
+			</div>
+		</div>
+	</div>
+
 </div>
