412a03ea9e8cb7adfccc043a81bfed112185ab6b
Ariful Islam <arif13414@gmail.com>
2023-08-20 16:15:04 +0600

    Fix layout issue of admin box content

--- assets/build/modules.asset.php
+++ assets/build/modules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '8c89c014ab8eb4c6260c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'b3fe64c6c55051fa268a');
