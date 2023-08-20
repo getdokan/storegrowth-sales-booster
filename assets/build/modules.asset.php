8cdb48c1b121ce2b234112e1705805a3448b2f3b
Ariful Islam <arif13414@gmail.com>
2023-08-20 15:47:14 +0600

    Fix ui issue of switch button

--- assets/build/modules.asset.php
+++ assets/build/modules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'e10dbd69fd17424f6822');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '8c89c014ab8eb4c6260c');
