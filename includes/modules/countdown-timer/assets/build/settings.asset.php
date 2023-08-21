7a5c45cdba9c9d80774495f177271657e724df7a
gooyeduck <dorhubs@gmail.com>
2023-08-21 11:11:25 +0600

    feat(countdown-timer): box shadow added

--- includes/modules/countdown-timer/assets/build/settings.asset.php
+++ includes/modules/countdown-timer/assets/build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => '0faf6d50b7c4c6756e6a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => '8ae654c4e1aa0a97e25d');
