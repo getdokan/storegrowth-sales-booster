cf478a54dfd2f0ff8df8d5858eaad98dd5281174
gooyeduck <dorhubs@gmail.com>
2023-09-02 00:49:46 +0600

    feat(admin-ui): last module deactivation set the page to initail form
    **Description**
    before the feature implementation when the last module is deactivated an warrning message is being shown.
    after this feature implementation when the last module is deactivate the page data gets to set its initial data at the page loading
    
    resolves: #197

--- assets/build/modules.asset.php
+++ assets/build/modules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '3b5728f6aa574e6527f4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '73d28a7685408f63e15a');
