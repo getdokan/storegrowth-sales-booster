ec6357d9a43e6b6fb2959a2d0591ba32180ed562
gooyeduck <dorhubs@gmail.com>
2023-09-11 18:14:29 +0600

    feat(free-bar): current build

--- includes/modules/progressive-discount-banner/assets/build/settings.asset.php
+++ includes/modules/progressive-discount-banner/assets/build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'fe9a7386cdbf0b22f4db');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '0f804eea6146e3051377');
--- includes/modules/progressive-discount-banner/assets/src/settings.js
+++ includes/modules/progressive-discount-banner/assets/src/settings.js
@@ -1,30 +1,41 @@
-import { addFilter } from '@wordpress/hooks';
-import DiscountBannerLayout from './components/DiscountBannerLayout';
-import { InsertRowAboveOutlined } from '@ant-design/icons';
+import { addFilter } from "@wordpress/hooks";
+import FreeShippingBarLayout from "./components/FreeShippingBarLayout";
+import { InsertRowAboveOutlined } from "@ant-design/icons";
 
 /**
  * Add routes to sidebar.
  */
-addFilter('sgsb_routes', 'sgsb', (routes) => {
-	routes.push({
-		path: '/progressive-discount-banner',
-		element: <DiscountBannerLayout />,
-		name: 'progressive-discount-banner',
-		label: 'Free Shipping Bar',
-	});
+addFilter(
+  "sgsb_routes",
+  "sgsb",
+  (routes, outlet, navigate, useParams, useSearchParams) => {
+    routes.push({
+      name: "progressive-discount-banner",
+      label: "Free Shipping Bar",
+      path: "/progressive-discount-banner",
+      element: (
+        <FreeShippingBarLayout
+          outlet={outlet}
+          navigate={navigate}
+          useParams={useParams}
+					useSearchParams={useSearchParams}
+        />
+      ),
+    });
 
-	return routes;
-});
+    return routes;
+  }
+);
 
 /**
  * Add sidebar menu items
  */
-addFilter('sidebar_menu_items', 'sgsb', (items, Link) => {
-	items.push({
-		label: <Link to="/progressive-discount-banner">Discount Banner</Link>,
-		key: 'progressive-discount-banner',
-		icon: <InsertRowAboveOutlined />,
-	});
+addFilter("sidebar_menu_items", "sgsb", (items, Link) => {
+  items.push({
+    label: <Link to="/progressive-discount-banner">Discount Banner</Link>,
+    key: "progressive-discount-banner",
+    icon: <InsertRowAboveOutlined />,
+  });
 
-	return items;
+  return items;
 });
