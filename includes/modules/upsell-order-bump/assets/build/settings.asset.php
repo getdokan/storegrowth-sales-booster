362604f993e09d2cee5d9ff58d0713244aea3c9c
MdAsifHossainNadim <nadim@wedevs.com>
2023-09-19 03:25:50 +0600

    fix: removed-redunded-codes-from-OrderBump.js

--- includes/modules/upsell-order-bump/assets/build/settings.asset.php
+++ includes/modules/upsell-order-bump/assets/build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e8707cc663ee25d41710');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '6d73378ce2398775c67c');
