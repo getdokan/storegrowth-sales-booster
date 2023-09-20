0c71af4e56988af1da4e11a7b002211f86953100
gooyeduck <dorhubs@gmail.com>
2023-09-20 12:45:10 +0600

    Stop tracking assets/build directory

--- assets/build/modules.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => '3c7a9fc75e7ca8f9890f');
--- assets/build/settings.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => '10f512725e2be023f661');
