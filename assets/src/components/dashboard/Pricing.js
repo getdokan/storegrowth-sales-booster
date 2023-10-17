e75a596b9e30bd23cd7347c8c498ecbaf3f97d58
gooyeduck <dorhubs@gmail.com>
2023-10-17 12:04:00 +0600

    fix(admin): pricing table updated and review removed
    **Description**
    In this commit the pricing table is updated accoding to the new desing and the review stars are also being removed.
    
    resolves: #274

--- assets/src/components/dashboard/Pricing.js
+++ assets/src/components/dashboard/Pricing.js
@@ -6,7 +6,7 @@ import DashboardTabs from "./DashboardTabs";
 import Promotion from "./Promotion";
 import PanelContainer from "../settings/Panels/PanelContainer";
 import PanelRow from "../settings/Panels/PanelRow";
-import {__} from "@wordpress/i18n";
+import { __ } from "@wordpress/i18n";
 
 const Pricing = () => {
   return (
@@ -26,7 +26,7 @@ const Pricing = () => {
                   {/* Render dashboard contents. */}
                   <div className="dashboad-content">
                     <div className="comparison-section">
-                      <div className="sg_pro_offer">
+                    {/* <div className="sg_pro_offer">
                         <div className="sg_pro_btm">
                           <div className="sg_pro_btm_left">
                             <div className="sg_pro_top">
@@ -102,7 +102,7 @@ const Pricing = () => {
                                     />
                                     Frontend order management
                                   </li>
-                                  {/* Add more list items as needed */}
+                                 
                                 </ul>
                               </div>
                               <div className="sg_pro_chart_2">
@@ -174,100 +174,113 @@ const Pricing = () => {
                                     />
                                     Frontend order management
                                   </li>
-                                  {/* Add more list items as needed */}
                                 </ul>
                               </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-
+                      </div> */}
                       {/* Package Table */}
                       <div className="sg_pricing_table">
                         <div className="sg_pricing_bottom">
                           <h3>The Package We Provide</h3>
-                          <a href="#">
-                            <span>most popular</span>
-                          </a>
                         </div>
                         <div className="sg_package">
                           <table>
                             <colgroup>
-                              <col span="1" className="bg" />
+                              {/* <col span="1" className="bg" /> */}
                               <col span="1" className="bg_2" />
-                              <col span="1" className="bg_3" />
+                              <col
+                                span="1"
+                                className="sg-most-popular-package-coloumn"
+                              />
                               <col span="1" className="bg_4" />
                             </colgroup>
                             <thead>
                               <tr className="row_1">
-                                <th style={{ borderBottom: "none" }}></th>
-                                <th style={{ borderBottom: "none" }}>
+                                {/* <th style={{ borderBottom: "none" }}></th> */}
+                                <th className="sgsb-pricing-table-header">
                                   <div className="sg_package_rate">
+                                    <span className="sg-package-title">
+                                      Personal Package
+                                    </span>
+                                    <a href="#">
+                                      <span className="buy-button-normal">
+                                        get started
+                                      </span>
+                                    </a>
                                     <h3>
                                       $49 <span>/ year</span>
                                     </h3>
-                                    <a href="#">
-                                      <span className="span">starter</span>
-                                    </a>
                                   </div>
                                 </th>
-                                <th style={{ borderBottom: "none" }}>
+                                <th className="sgsb-pricing-table-header-popular">
                                   <div className="sg_package_rate">
+                                    <span className="sgsb-popular-packages">
+                                      most popular packages
+                                    </span>
+                                    <span className="sg-package-title">
+                                      Agency Package
+                                    </span>
+                                    <a href="#">
+                                      <span className="buy-button-popular">
+                                        get started
+                                      </span>
+                                    </a>
                                     <h3>
                                       $89 <span>/ year</span>
                                     </h3>
-                                    <a href="#">
-                                      <span className="span_2">
-                                        professional
-                                      </span>
-                                    </a>
                                   </div>
                                 </th>
-                                <th style={{ borderBottom: "none" }}>
+                                <th className="sgsb-pricing-table-header">
                                   <div className="sg_package_rate">
+                                    <span className="sg-package-title">
+                                      Lifetime Package
+                                    </span>
+                                    <a href="#">
+                                      <span className="buy-button-normal">
+                                        get started
+                                      </span>
+                                    </a>
                                     <h3>
                                       $159 <span>/ year</span>
                                     </h3>
-                                    <a href="#">
-                                      <span className="span_3">business</span>
-                                    </a>
                                   </div>
                                 </th>
                               </tr>
                             </thead>
                             <tbody>
                               <tr>
-                                <td>website</td>
+                                {/* <td>website</td> */}
                                 <td>01</td>
                                 <td>09</td>
                                 <td>03</td>
                               </tr>
                               <tr>
-                                <td>ready demo</td>
+                                {/* <td>ready demo</td> */}
                                 <td>05</td>
                                 <td>03</td>
                                 <td>06</td>
                               </tr>
                               <tr>
-                                <td>domain</td>
+                                {/* <td>domain</td> */}
                                 <td>08</td>
                                 <td>01</td>
                                 <td>05</td>
                               </tr>
                               <tr>
-                                <td>color scheme</td>
+                                {/* <td>color scheme</td> */}
                                 <td>01</td>
                                 <td>05</td>
                                 <td>10</td>
                               </tr>
                               <tr>
-                                <td>action</td>
+                                {/* <td>action</td> */}
                                 <td>02</td>
                                 <td>07</td>
                                 <td>03</td>
                               </tr>
-                              <tr>
-                                <td style={{ borderBottom: "none" }}></td>
+                              {/* <tr>
                                 <td style={{ borderBottom: "none" }}>
                                   <div className="sg_buy">
                                     <a href="#">
@@ -289,7 +302,7 @@ const Pricing = () => {
                                     </a>
                                   </div>
                                 </td>
-                              </tr>
+                              </tr> */}
                             </tbody>
                           </table>
                         </div>
