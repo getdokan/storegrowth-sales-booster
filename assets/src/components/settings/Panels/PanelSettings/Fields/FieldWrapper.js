0cde80e177cedca43de8c246bdea5d9a0cc137e0
gooyeduck <dorhubs@gmail.com>
2023-11-12 20:38:59 +0600

    fix(sales-countdown): instruction data and width
    In this commit the instruction of the sales countdown and the width issue in frontend is fixed.
    
    resolves: Plugin Issues: Release Candidate #287

--- assets/src/components/settings/Panels/PanelSettings/Fields/FieldWrapper.js
+++ assets/src/components/settings/Panels/PanelSettings/Fields/FieldWrapper.js
@@ -1,18 +1,24 @@
-import { Card, Row, Col } from 'antd';
+import { Card, Row, Col } from "antd";
 import UpgradeOverlay from "../UpgradeOverlay";
 
-const FieldWrapper = ( { colSpan, children, upgradeClass, align = 'top' } ) => {
+const FieldWrapper = ({
+    colSpan,
+    children,
+    upgradeClass,
+    align = "top",
+    justify = "center",
+}) => {
     return (
         // Settings field wrapper component with card preview.
-        <Col className="gutter-row" span={ colSpan }>
-            <Card className={ `sgsb-settings-card ${ upgradeClass }` }>
-                <Row justify="center" align={ align }>
-                    { children }
+        <Col className="gutter-row" span={colSpan}>
+            <Card className={`sgsb-settings-card ${upgradeClass}`}>
+                <Row justify={justify} align={align}>
+                    {children}
                 </Row>
-                { upgradeClass && <UpgradeOverlay /> }
+                {upgradeClass && <UpgradeOverlay />}
             </Card>
         </Col>
     );
-}
+};
 
 export default FieldWrapper;
---
+++ includes/modules/countdown-timer/assets/src/components/SettingInstruction.js
@@ -0,0 +1,60 @@
+import Image from "antd/es/image";
+import React from "react";
+import scInstruction from "../../images/sales-countdown-meta.png";
+import FieldWrapper from "sales-booster/src/components/settings/Panels/PanelSettings/Fields/FieldWrapper";
+
+const SettingInstruction = () => {
+  return (
+    <FieldWrapper colSpan={24} justify={`start`}>
+      <div>
+        <p>
+          To set the countdown and discount of a specific product must go to{" "}
+          <a href={`/wp-admin/edit.php?post_type=product`}>
+            <strong>all products</strong>
+          </a>{" "}
+          in woocommerce. Then select a desired product to <b>Edit</b>. In the
+          product meta of woocommerce you will be able to see{" "}
+          <b>"Sales Countdown"</b>.
+        </p>
+        <Image preview={false} src={scInstruction} />
+        <div
+          style={{
+            marginTop: "10px",
+          }}
+        >
+          <p>
+            Explore full range of products{" "}
+            <a href={`/wp-admin/edit.php?post_type=product`}>
+              <strong>here</strong>
+            </a>
+            .
+          </p>
+        </div>
+        <div>
+          <button
+            style={{
+              backgroundColor: "#3498db",
+              color: "#fff",
+              padding: "10px 20px",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              display: "block",
+              margin: "10px 0",
+            }}
+            onClick={() =>
+              window.open(
+                "https://storegrowth.io/docs/sales-countdown/",
+                "_blank"
+              )
+            }
+          >
+            Documentaion
+          </button>
+        </div>
+      </div>
+    </FieldWrapper>
+  );
+};
+
+export default SettingInstruction;
