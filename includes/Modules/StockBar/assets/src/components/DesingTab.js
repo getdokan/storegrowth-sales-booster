c1ffc66ff9b47b7ad8b1280205e80ca84f4c253e
gooyeduck <dorhubs@gmail.com>
2023-12-04 12:06:25 +0600

    refactor(stock-bar): free pro code separation.
    In this commit the pro feature of the admin ui is separated using the filter hooks.

--- includes/Modules/StockBar/assets/src/components/DesingTab.js
+++ includes/Modules/StockBar/assets/src/components/DesingTab.js
@@ -1,10 +1,8 @@
 import { Fragment } from "react";
 import { __ } from "@wordpress/i18n";
+import { applyFilters } from '@wordpress/hooks';
 import SettingsSection from "sales-booster/src/components/settings/Panels/PanelSettings/SettingsSection";
 import ColourPicker from "sales-booster/src/components/settings/Panels/PanelSettings/Fields/ColorPicker";
-import Number from "sales-booster/src/components/settings/Panels/PanelSettings/Fields/Number";
-import SelectBox from "sales-booster/src/components/settings/Panels/PanelSettings/Fields/SelectBox";
-import TextInput from "sales-booster/src/components/settings/Panels/PanelSettings/Fields/TextInput";
 import ActionsHandler from "sales-booster/src/components/settings/Panels/PanelSettings/ActionsHandler";
 import Templates from "./Templates";
 
@@ -15,21 +13,8 @@ function DesignTab(props) {
     onFieldChange,
     onFormSave,
     buttonLoading,
-    upgradeTeaser,
     onFormReset,
-    noop,
   } = props;
-
-  const barDisplayFormat = [
-    {
-      value: "above",
-      label: __("Above Stock Bar", "storegrowth-sales-booster"),
-    },
-    {
-      value: "below",
-      label: __("Below Stock Bar", "storegrowth-sales-booster"),
-    },
-  ];
 
   return (
     <Fragment>
@@ -40,13 +25,12 @@ function DesignTab(props) {
           changeHandler={onFieldChange}
           title={__("Foreground Color", "storegrowth-sales-booster")}
         />
-        <ColourPicker
-          needUpgrade={upgradeTeaser}
-          name={"stockbar_fg_color"}
-          fieldValue={formData.stockbar_fg_color}
-          changeHandler={upgradeTeaser ? noop : onFieldChange}
-          title={__("Bar Color", "storegrowth-sales-booster")}
-        />
+        { applyFilters(
+          'sgsb_bar_color_stock_bar_settings',
+          '',
+          formData,
+          onFieldChange
+        ) } 
         <ColourPicker
           name={"stockbar_border_color"}
           fieldValue={formData.stockbar_border_color}
@@ -54,64 +38,12 @@ function DesignTab(props) {
           title={__("Border Color", "storegrowth-sales-booster")}
         />
 
-        <Number
-          min={1}
-          max={100}
-          style={{
-            width: "100px",
-          }}
-          name={`stockbar_height`}
-          changeHandler={upgradeTeaser ? noop : onFieldChange}
-          fieldValue={formData.stockbar_height}
-          needUpgrade={upgradeTeaser}
-          title={__(`Stock Bar Height`, "storegrowth-sales-booster")}
-        />
-
-        <SelectBox
-          needUpgrade={upgradeTeaser}
-          name={`stock_display_format`}
-          options={[...barDisplayFormat]}
-          fieldValue={formData.stock_display_format}
-          changeHandler={upgradeTeaser ? noop : onFieldChange}
-          title={__("Stock Display Format", "storegrowth-sales-booster")}
-          tooltip={__(
-            "Select your desired font family",
-            "storegrowth-sales-booster"
-          )}
-        />
-
-        <TextInput
-          name={"total_sell_count_text"}
-          placeHolderText={__(
-            "Last chance! [discount]% OFF",
-            "storegrowth-sales-booster"
-          )}
-          fieldValue={formData.total_sell_count_text}
-          className={`settings-field input-field`}
-          changeHandler={upgradeTeaser ? noop : onFieldChange}
-          title={__("Total Sell Count Text", "storegrowth-sales-booster")}
-          tooltip={__(
-            "It will be placed left side of the above of the Stock Bar. e.g. Total Sold",
-            "storegrowth-sales-booster"
-          )}
-          needUpgrade={upgradeTeaser}
-        />
-        <TextInput
-          name={"available_item_count_text"}
-          placeHolderText={__(
-            "Last chance! [discount]% OFF",
-            "storegrowth-sales-booster"
-          )}
-          fieldValue={formData.available_item_count_text}
-          className={`settings-field input-field`}
-          changeHandler={upgradeTeaser ? noop : onFieldChange}
-          title={__("Available Item Count Text", "storegrowth-sales-booster")}
-          tooltip={__(
-            "It will be placed right side of the above of the Stock Bar. e.g. Available Item",
-            "storegrowth-sales-booster"
-          )}
-          needUpgrade={upgradeTeaser}
-        />
+        { applyFilters(
+          'sgsb_design_panel_stock_bar_settings',
+          '',
+          formData,
+          onFieldChange
+        ) } 
       </SettingsSection>
 
       <Templates
