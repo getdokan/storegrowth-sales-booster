704e9121ccff5545323f33db746342830f399f18
Satyajit Talukder <65698588+gooyeduck@users.noreply.github.com>
2024-01-09 16:10:32 +0600

    fix(direct-checkout): buy now button in block theme.
    In this theme the buy now button is being fixed. Before the fix in the block theme the buy now button was not working perfectly after the fix the buy now button is now working.
    
    resolves: #346

--- Includes/Modules/DirectCheckout/Includes/CommonHooks.php
+++ Includes/Modules/DirectCheckout/Includes/CommonHooks.php
@@ -47,16 +47,54 @@ class CommonHooks {
 				add_action( 'woocommerce_process_product_meta', array( $this, 'save_direct_checkout_data' ) );
 
 				// Cart button as buy now button.
+				add_filter( 'woocommerce_loop_add_to_cart_link', array( $this, 'show_signle_direct_checkout_button_shop' ), 15 );
 				add_filter( 'wc_get_template', array( $this, 'set_cart_to_checkout_button_template' ), 10, 5 );
 				add_filter( 'woocommerce_locate_template', array( $this, 'set_template_path' ), 10, 3 );
 			}
 		} elseif ( 'cart-to-buy-now' === $buy_now_button_setting ) {
 			// Modify cart to checkout button template.
+			add_filter( 'woocommerce_loop_add_to_cart_link', array( $this, 'show_signle_direct_checkout_button_shop' ), 15 );
 			add_filter( 'wc_get_template', array( $this, 'set_cart_to_checkout_button_template' ), 10, 5 );
 			add_filter( 'woocommerce_locate_template', array( $this, 'set_template_path' ), 10, 3 );
 		} else {
 			return;
 		}
+	}
+
+	/**
+	 * Hook for WooCommerce loop add to cart link.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $add_to_cart Add to cart link.
+	 *
+	 * @return string
+	 */
+	public function show_signle_direct_checkout_button_shop( $add_to_cart ) {
+
+		if ( $this->should_display_buy_now_button( 'shop_page_checkout_enable' ) ) {
+			ob_start();
+			global $product;
+			$product_id                    = get_the_ID();
+			$direct_checkout_button_layout = get_post_meta( $product_id, '_sgsb_direct_checkout_button_layout', true );
+			$settings                      = get_option( 'sgsb_direct_checkout_settings' );
+			$buy_now_button_setting        = sgsb_find_option_setting( $settings, 'buy_now_button_setting', 'cart-to-buy-now' );
+			if (
+				( 'cart-to-buy-now' === $direct_checkout_button_layout && 'specific-buy-now' === $buy_now_button_setting )
+				|| 'cart-to-buy-now' === $buy_now_button_setting
+				) {
+				if ( 'simple' === $product->get_type() && $product->is_purchasable() && $product->is_in_stock() ) {
+					include __DIR__ . '/../templates/buy-now-button-template.php';
+					$buy_now_button = ob_get_contents();
+					ob_end_clean();
+
+					$add_to_cart = $buy_now_button;
+
+					return $add_to_cart;
+				}
+			}
+		}
+		return $add_to_cart;
 	}
 
 	/**
@@ -161,8 +199,8 @@ class CommonHooks {
 		$buy_now_button_setting        = sgsb_find_option_setting( $settings, 'buy_now_button_setting', 'cart-with-buy-now' );
 
 		if (
-			( 'cart-to-buy-now' === $direct_checkout_button_layout && 'specific-buy-now' === $buy_now_button_setting )
-			|| 'cart-to-buy-now' === $buy_now_button_setting
+		( 'cart-to-buy-now' === $direct_checkout_button_layout && 'specific-buy-now' === $buy_now_button_setting )
+		|| 'cart-to-buy-now' === $buy_now_button_setting
 		) {
 			if ( in_array( $template_name, array( 'single-product/add-to-cart/simple.php', 'loop/add-to-cart.php' ), true ) ) {
 				$template = __DIR__ . '/../templates/add-cart-buy-now.php';
@@ -209,7 +247,7 @@ class CommonHooks {
 		|| 'cart-with-buy-now' === $buy_now_button_setting
 		) {
 			if ( 'simple' === $product->get_type() && $product->is_purchasable() && $product->is_in_stock() ) {
-				include __DIR__ . '/../templates/add-cart-with-buy-now.php';
+				include __DIR__ . '/../templates/buy-now-button-template.php';
 			}
 		}
 	}
