27f3774f341b6b60a7cf6b3bca1d8aa75d1dc83b
Satyajit Talukder <65698588+gooyeduck@users.noreply.github.com>
2024-01-18 17:11:19 +0600

    fix(project): admin menu fix and refactored the code

--- Includes/Admin/AdminHooks.php
+++ Includes/Admin/AdminHooks.php
@@ -10,55 +10,53 @@ namespace STOREGROWTH\SPSB\Admin;
 use STOREGROWTH\SPSB\Traits\Singleton;
 
 // If this file is called directly, abort.
-if (! defined('ABSPATH') ) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 /**
  * Add misc dashbaord end hooks inside this class.
  */
-class AdminHooks
-{
+class AdminHooks {
 
-    use Singleton;
 
-    /**
-     * Constructor of Admin_Hooks class.
-     */
-    private function __construct()
-    {
-        add_filter('plugin_action_links_' . STOREGROWTH_PLUGIN_BASENAME, array( $this, 'plugin_action_links' ));
-        add_action('admin_init', [ $this, 'sgsb_redirect_initial_setup' ]);
-    }
+	use Singleton;
 
-    /**
-     * Show action links on the plugin screen.
-     *
-     * @param mixed $links Plugin Action links.
-     *
-     * @return array
-     */
-    public function plugin_action_links( $links )
-    {
-        $action_links = array(
-        'dashboard' => '<a href="' . admin_url('admin.php?page=sgsb-settings#/dashboard/overview') . '">' . esc_html__('Dashboard', 'storegrowth-sales-booster') . '</a>',
-        'modules'   => '<a href="' . admin_url('admin.php?page=sgsb-modules') . '">' . esc_html__('Modules', 'storegrowth-sales-booster') . '</a>',
-        'settings'  => '<a href="' . admin_url('admin.php?page=sgsb-settings') . '">' . esc_html__('Settings', 'storegrowth-sales-booster') . '</a>',
-        );
+	/**
+	 * Constructor of Admin_Hooks class.
+	 */
+	private function __construct() {
+		add_filter( 'plugin_action_links_' . STOREGROWTH_PLUGIN_BASENAME, array( $this, 'plugin_action_links' ) );
+		add_action( 'admin_init', array( $this, 'sgsb_redirect_initial_setup' ) );
+	}
 
-        return array_merge($action_links, $links);
-    }
+	/**
+	 * Show action links on the plugin screen.
+	 *
+	 * @param mixed $links Plugin Action links.
+	 *
+	 * @return array
+	 */
+	public function plugin_action_links( $links ) {
+		$action_links = array(
+			'dashboard' => '<a href="' . admin_url( 'admin.php?page=sgsb-settings#/dashboard/overview' ) . '">' . esc_html__( 'Dashboard', 'storegrowth-sales-booster' ) . '</a>',
+			'modules'   => '<a href="' . admin_url( 'admin.php?page=sgsb-modules' ) . '">' . esc_html__( 'Modules', 'storegrowth-sales-booster' ) . '</a>',
+			'settings'  => '<a href="' . admin_url( 'admin.php?page=sgsb-settings' ) . '">' . esc_html__( 'Settings', 'storegrowth-sales-booster' ) . '</a>',
+		);
 
-    /**
-     * Redirect to Welcome Page when plugin is activated.
-     */
+		return array_merge( $action_links, $links );
+	}
 
-    public function sgsb_redirect_initial_setup()
-    {
-        if (get_option('storegrowth_activation_redirect', false)) {
-            delete_option('storegrowth_activation_redirect');
-            if(wp_safe_redirect(admin_url('admin.php?page=sgsb-modules#/ini-setup'))) { exit();
-            }
-        }
-    }
+	/**
+	 * Redirect to Welcome Page when plugin is activated.
+	 */
+
+	public function sgsb_redirect_initial_setup() {
+		if ( get_option( 'storegrowth_activation_redirect', false ) ) {
+			delete_option( 'storegrowth_activation_redirect' );
+			if ( wp_safe_redirect( admin_url( 'admin.php?page=sgsb-modules#/ini-setup' ) ) ) {
+				exit();
+			}
+		}
+	}
 }
--- assets/src/utils/helper.js
+++ assets/src/utils/helper.js
@@ -1,78 +1,107 @@
 import { __ } from '@wordpress/i18n';
 import { Ajax } from '../ajax';
 
-export const menuFix = ( slug ) => {
+export const menuFix = (slug) => {
     const $ = jQuery;
 
     const location = window.location,
         hash = location.hash,
-        menuRoot = $( '#toplevel_page_' + slug );
+        menuRoot = $('#toplevel_page_' + slug);
 
-    const dashboardHashRoutes = [ '#/dashboard/overview', '#/dashboard/pricing', '#/dashboard/faq' ];
+    const dashboardHashRoutes = ['#/dashboard/overview', '#/dashboard/pricing', '#/dashboard/faq'];
+
+    function adminMenuRemover() { 
+        $("#wpadminbar , #adminmenumain").remove();
+    
+        $(".wp-toolbar").css({
+          padding: "0", // Replace with your desired background color
+        });
+        $("#wpcontent, #wpfooter").css({
+            marginLeft:"0",
+          background: "#fff", // Replace with your desired background color
+        });
+        $(".notice").remove();
+    }
+    // *************************************
+    // Add target blank for upgrade button
+    // *************************************
+    $("#toplevel_page_sales-booster-for-woocommerce ul > li > a").each(function (e) {
+        if ($(this).attr("href").indexOf("?page=go-sgsb-pro") > 0) {
+            $(this).attr("target", "_blank");
+            // Add hover effect
+            $(this).hover(
+                function () {
+                    $(this).css("color", "yellowgreen");
+                },
+                function () {
+                    $(this).css("color", ""); // Revert to original color on hover out
+                }
+            );
+        }
+        if ($(this).attr("href").indexOf("?page=go-sgsb-docs") > 0) {
+            $(this).attr("target", "_blank");
+        }
+    });
 
     // Handle dashboard activation slug for first time load.
-    if ( dashboardHashRoutes.includes( hash ) ) {
-        $( 'ul.wp-submenu li', menuRoot ).removeClass( 'current' );
-        menuRoot.find( `li.wp-first-item` ).addClass( 'current' );
+    if (dashboardHashRoutes.includes(hash)) {
+        $('ul.wp-submenu li', menuRoot).removeClass('current');
+        menuRoot.find(`li.wp-first-item`).addClass('current');
     }
 
     const currentUrl = location.href;
     // Handle activation slug for overall submenus.
-    menuRoot.on( 'click', 'a', function () {
-        const self = $( this );
+    menuRoot.on('click', 'a', function () {
+        const self = $(this);
 
-        $( 'ul.wp-submenu li', menuRoot ).removeClass( 'current' );
+        $('ul.wp-submenu li', menuRoot).removeClass('current');
 
         // Check if submenu class then active.
-        if ( self.hasClass( 'wp-has-submenu' ) ) {
-            $( 'li.wp-first-item', menuRoot ).addClass( 'current' );
+        if (self.hasClass('wp-has-submenu')) {
+            $('li.wp-first-item', menuRoot).addClass('current');
         } else {
-            self.parents( 'li' ).addClass( 'current' );
+            self.parents('li').addClass('current');
         }
-    } );
+    });
 
     // Handle menu activation by submenu hyper ref checking.
-    $( 'ul.wp-submenu a', menuRoot ).each( function ( index, el ) {
-        if ( $( el ).attr( 'href' ) === currentUrl ) {
-            $( el ).parent().addClass( 'current' );
+    $('ul.wp-submenu a', menuRoot).each(function (index, el) {
+        if ($(el).attr('href') === currentUrl) {
+            $(el).parent().addClass('current');
         }
-    } );
+    });
 
-    menuRoot.on( 'click', 'a', function () {
-        const self = $( this );
+    if ("#/ini-setup" === hash || "#ini-setup" === hash) {
+        adminMenuRemover();
+    }
+    
+    menuRoot.on('click', 'a', function () {
+        const self = $(this);
         const uri = self.attr('href');
         const hash = uri.split('#')[1];
 
-        if("/ini-setup"===hash){
-            console.log("properly getting the hash");
-            $("#wpadminbar , #adminmenumain").remove();
-            $(".wp-toolbar").css({
-            padding: "0", // Replace with your desired background color
-            });
-            $("#wpcontent, #wpfooter").css({
-            marginLeft:"0", // Replace with your desired background color
-            });
-            $(".notice").remove();
+        if ("/ini-setup" === hash) {
+            adminMenuRemover();
         }
-    } );
+    });
 }
 
-export const extractedTitle = ( title, length = 60 ) => {
-    const extractedString = title?.substr( 0, length );
+export const extractedTitle = (title, length = 60) => {
+    const extractedString = title?.substr(0, length);
     return extractedString?.length >= length
-        ? `${ extractedString }...`
+        ? `${extractedString}...`
         : extractedString;
 }
 
-export const wpMedia = ( {
+export const wpMedia = ({
     callback,
-    title = __( 'Select or upload media.', 'storegrowth-sales-booster' ),
+    title = __('Select or upload media.', 'storegrowth-sales-booster'),
     selectMultiple = false,
-    selectText = __( 'Select', 'storegrowth-sales-booster' ),
+    selectText = __('Select', 'storegrowth-sales-booster'),
     fileType = 'image',
     dimension = {},
-} ) => {
-    const frame = wp.media( {
+}) => {
+    const frame = wp.media({
         title,
         button: {
             text: selectText,
@@ -81,20 +110,20 @@ export const wpMedia = ( {
             type: fileType,
         },
         multiple: selectMultiple, // Set to true to allow multiple files to be selected
-    } );
+    });
 
     frame.open();
 
     // When an file is selected, run a callback.
-    frame.on( 'select', () => {
-        const attachment = frame.state().get( 'selection' ).first().toJSON();
+    frame.on('select', () => {
+        const attachment = frame.state().get('selection').first().toJSON();
 
-        callback( attachment );
-    } );
+        callback(attachment);
+    });
 }
 
-export const deactivatorHandler= (moduleID)=>{
-    Ajax( 'update_module_status', {
+export const deactivatorHandler = (moduleID) => {
+    Ajax('update_module_status', {
         module_id: moduleID,
         status: false
     }).success((response) => {
@@ -103,15 +132,15 @@ export const deactivatorHandler= (moduleID)=>{
             window.location.href = sgsbSettingsURL;
         }
     });
-    
+
 }
 
 export const removeHashFromURL = (url) => {
     if (url.startsWith('#/')) {
-      return url.slice(2); // Remove "#/"
+        return url.slice(2); // Remove "#/"
     } else if (url.startsWith('#')) {
-      return url.slice(1); // Remove "#"
+        return url.slice(1); // Remove "#"
     } else {
-      return url; // No change needed
+        return url; // No change needed
     }
 }
