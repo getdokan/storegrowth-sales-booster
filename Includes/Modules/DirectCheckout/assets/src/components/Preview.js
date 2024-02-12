e1bc5b19f827a6075771f9f8561e609de5e3a31d
MdAsifHossainNadim <nadim@wedevs.com>
2024-02-13 01:46:20 +0600

    enhance: implement-buy-now-button-ui-customization-functionalities-for-direct-checkout-module

--- Includes/Modules/DirectCheckout/assets/src/components/Preview.js
+++ Includes/Modules/DirectCheckout/assets/src/components/Preview.js
@@ -2,17 +2,20 @@ import { Button } from "antd";
 const Preview = ({storeData}) => {
   return (
     <div>
-      <Button type="primary" size={"large"}
-      style={{
-        color:storeData?.text_color,
-        borderRadius:storeData?.button_border_radius,
-        borderColor:storeData?.button_color,
-        fontSize:storeData?.font_size,
-        backgroundColor:storeData?.button_color,
-        height:"fit-content",
-        padding:"10px 30px",
-        fontWeight:"600"
-      }}
+      <Button
+        type="primary"
+        size={"large"}
+        style={{
+          color           : storeData?.text_color,
+          border          : `${storeData?.border_width}px ${storeData?.button_border_style} ${storeData?.border_color}`,
+          height          : "fit-content",
+          padding         : `${storeData?.paddingYaxis}px ${storeData?.paddingXaxis}px`,
+          fontSize        : storeData?.font_size,
+          fontWeight      : "600",
+          fontFamily      : storeData?.font_family,
+          borderRadius    : storeData?.button_border_radius,
+          backgroundColor : storeData?.button_color
+        }}
       >
         {storeData?.buy_now_button_label === "" ? "Enter Text" : storeData?.buy_now_button_label}
       </Button>
