63f2dccb085f2b7a0d3e93b476cf312f81842e3f
Satyajit Talukder <dorhubs@gmail.com>
2024-02-12 17:29:52 +0600

    feat(block): registered custom categories

--- Includes/Modules/CountdownTimer/assets/src/blocks/SalesCountdown.js
+++ Includes/Modules/CountdownTimer/assets/src/blocks/SalesCountdown.js
@@ -2,21 +2,17 @@ import { registerBlockType } from '@wordpress/blocks';
 import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
-
 import { ReactComponent as CountdownIcon } from '../../images/countdown-timer.svg'; // Import SVG icon correctly
 
+
+// Register your block type
 registerBlockType(
   metadata.name,
   {
     title: "Sales Countdown",
+    category: "sales-booster", // Assign the category to your block
     icon: <CountdownIcon />, // Use the SVG icon directly
-    /**
-     * @see ./edit.js
-     */
     edit: Edit,
-    /**
-     * @see ./save.js
-     */
     save,
   }
 );
