64eb04da1cc7c5123b623c482ef537e21cc16d40
Satyajit Talukder <dorhubs@gmail.com>
2024-04-09 10:01:21 +0600

    feat(SSB-79): variation product addition

--- Includes/Modules/UpsellOrderBump/Includes/OrderBump.php
+++ Includes/Modules/UpsellOrderBump/Includes/OrderBump.php
@@ -35,23 +35,30 @@ class OrderBump {
 	public function bump_product_frontend_view() {
 
 		global $woocommerce;
-		$all_cart_products      = $woocommerce->cart->get_cart();
-		$all_cart_product_ids   = array();
-		$all_cart_category_ids  = array();
-		$args_bump              = array(
+		$all_cart_products     = $woocommerce->cart->get_cart();
+		$all_cart_product_ids  = array();
+		$all_cart_category_ids = array();
+		$args_bump             = array(
 			'post_type'      => 'sgsb_order_bump',
 			'posts_per_page' => -1,
 		);
-		$bump_list              = get_posts( $args_bump );
+		$bump_list             = get_posts( $args_bump );
 
 		foreach ( $all_cart_products as $value ) {
+			error_log( $value['data']->is_type( 'simple' ) );
 			$cat_ids = $value['data']->get_category_ids();
 			foreach ( $cat_ids as $cat_id ) {
 				$all_cart_category_ids[] = $cat_id;
 			}
-			$all_cart_product_ids[] = $value['product_id'];
+				// $all_cart_product_ids[] = array(
+				// 	'product_id'   => $value['product_id'],
+				// 	'variation_id' => $value['variation_id'],
+				// 	'variation'    => $value['variation'],
+				// );
+				$all_cart_product_ids[] = $value['variation_id'] === 0 ? $value['product_id'] : $value['variation_id'];
 		}
-
+		error_log( print_r( $all_cart_product_ids, 1 ) );
+		// error_log( print_r( $all_cart_products, 1 ) );
 		foreach ( $bump_list as $bump ) {
 			$bump_info        = maybe_unserialize( $bump->post_excerpt );
 			$bump_info        = (object) $bump_info;
@@ -91,18 +98,18 @@ class OrderBump {
 				continue;
 			}
 
-            $bump_type = ! empty( $bump_info->bump_type ) ? esc_html( $bump_info->bump_type ) : 'products';
-            if ( $bump_type === 'products' ) {
-                $target_products = ! empty( $bump_info->target_products ) ? wc_clean( $bump_info->target_products ) : array();
-                if ( $target_products && ( count( $all_cart_product_ids ) !== count( array_diff( $all_cart_product_ids, $target_products ) ) ) ) {
-                    include __DIR__ . '/../templates/bump-product-front-view.php';
-                }
-            } else {
-                $target_categories = ! empty( $bump_info->target_categories ) ? wc_clean( $bump_info->target_categories ) : array();
-                if ( $target_categories && ( count( $all_cart_category_ids ) !== count( array_diff( $all_cart_category_ids, $target_categories ) ) ) ) {
-                    include __DIR__ . '/../templates/bump-product-front-view.php';
-                }
-            }
+			$bump_type = ! empty( $bump_info->bump_type ) ? esc_html( $bump_info->bump_type ) : 'products';
+			if ( $bump_type === 'products' ) {
+				$target_products = ! empty( $bump_info->target_products ) ? wc_clean( $bump_info->target_products ) : array();
+				if ( $target_products && ( count( $all_cart_product_ids ) !== count( array_diff( $all_cart_product_ids, $target_products ) ) ) ) {
+					include __DIR__ . '/../templates/bump-product-front-view.php';
+				}
+			} else {
+				$target_categories = ! empty( $bump_info->target_categories ) ? wc_clean( $bump_info->target_categories ) : array();
+				if ( $target_categories && ( count( $all_cart_category_ids ) !== count( array_diff( $all_cart_category_ids, $target_categories ) ) ) ) {
+					include __DIR__ . '/../templates/bump-product-front-view.php';
+				}
+			}
 		}
 	}
 
