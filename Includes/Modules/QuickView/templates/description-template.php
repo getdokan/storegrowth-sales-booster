82cc658469798f312038da6c7b76e54a18c68a4d
satyajittalukder <dorhubs@gmail.com>
2024-09-13 03:02:58 +0600

    fix: description template fix for rich text

--- Includes/Modules/QuickView/templates/description-template.php
+++ Includes/Modules/QuickView/templates/description-template.php
@@ -1,8 +1,10 @@
-<div class="description-container">
-	<div class="description-heading">
-	<h3 class="heading">Description</h3>
-	</div>
-	<div class="description-content">
-	<?php echo esc_attr( $description ); ?>
-	</div>
-</div>
+<?php 
+global $post;
+$heading = apply_filters( 'sgsb_product_description_heading', __( 'Description', 'woocommerce' ) );
+?>
+
+<?php if ( $heading ) : ?>
+	<h2><?php echo esc_html( $heading ); ?></h2>
+<?php endif; ?>
+
+<?php the_content(); ?>
