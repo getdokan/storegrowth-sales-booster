e5b21eb9316f2c5c8ab4dd981eab5558802c5205
Mahbub Rabbani <mahbub.rucse@gmail.com>
2025-08-14 13:58:43 +0600

    Refactor: league container with mozart (#449)
    * refactor: Add mozart
    
    * fix: file autoloading

--- includes/functions.php
+++ includes/functions.php
@@ -5,11 +5,6 @@
  * @package WPBP
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
 if ( ! function_exists( 'sgsb_assets_url' ) ) {
 	/**
 	 * Get plugin assets url.
