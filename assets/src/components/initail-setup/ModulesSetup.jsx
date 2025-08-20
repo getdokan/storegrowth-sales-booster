2326fdf4aca2d97ae010a8138db6b1ba69d09926
Aunshon <32583103+Aunshon@users.noreply.github.com>
2025-08-20 17:19:23 +0600

    refactor: remove pagination logic from module setup

--- assets/src/components/initail-setup/ModulesSetup.jsx
+++ assets/src/components/initail-setup/ModulesSetup.jsx
@@ -1,49 +1,16 @@
 import { useDispatch, useSelect } from "@wordpress/data";
-import { useEffect, useState} from "@wordpress/element";
-import { Pagination } from "antd";
+import { useEffect } from "@wordpress/element";
 import ModuleList from "../modules/ModuleList";
 import { Ajax } from "../../ajax";
 import { __ } from "@wordpress/i18n";
 
 function ModulesSetup() {
-
-  const perPageItem = 6;
-
-  const [pagination, setPagination] = useState({
-    minValue: 0,
-    maxValue: perPageItem,
-    selectFilter: { modules: [] },
-    currentPage: 1,
-  });
-
   const { updateModules } = useDispatch("sgsb");
 
   // Get from WP data.
   const { allModules } = useSelect((select) => ({
     allModules: select("sgsb").getModules(),
   }));
-
-  const hanglePageItem = (pageNumber) => {
-    // Pagination calculation based on all Modules (preserves the current page)
-    const startIndex = (pageNumber - 1) * perPageItem;
-    const endIndex = startIndex + perPageItem;
-
-    setPagination({
-      ...pagination,
-      minValue: startIndex,
-      maxValue: endIndex,
-      currentPage: pageNumber,
-    });
-  };
-
-  useEffect(() => {
-    if (allModules) {
-      setPagination({
-        ...pagination,
-        selectFilter: { modules: allModules },
-      });
-    }
-  }, [allModules]);
 
   useEffect(() => {
     Ajax("get_all_modules").success((response) => {
@@ -62,24 +29,7 @@ function ModulesSetup() {
         </div>
         <ModuleList
           modules={allModules}
-          minValue={pagination?.minValue}
-          maxValue={pagination?.maxValue}
         />
-        <div
-          className="sgsb__module-pagination"
-          style={{
-            paddingLeft: "22px",
-          }}
-        >
-          <Pagination
-            defaultCurrent={1}
-            current={pagination?.currentPage}
-            defaultPageSize={perPageItem}
-            onChange={hanglePageItem}
-            total={allModules?.length}
-            hideOnSinglePage={false}
-          />
-        </div>
       </div>
     </div>
   );
