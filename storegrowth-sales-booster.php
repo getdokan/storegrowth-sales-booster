a28e8b90d0a5c445751a1278c6fd23107552efb5
Md Mahbub Rabbani <mahbub.rucse@gmail.com>
2025-09-05 10:12:46 +0600

    rename: Change the plugin name to 'StoreGrowth'

--- storegrowth-sales-booster.php
+++ storegrowth-sales-booster.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: StoreGrowth - Sales Booster For WooCommerce Lite
+ * Plugin Name: StoreGrowth
  * Description: Best WooCommerce Direct Checkout, Fly Cart, BOGO, Quick View, Live Sales Notifications, Floating Notification Bar and More Essential Features for Every WooCommerce Site!
  * Version:     1.28.13
  * Author:      Dokan Inc.
