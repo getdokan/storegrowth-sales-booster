5ee81512ac84ed7ad4c9b9f01e7db7dafb210fec
Kamruzzaman <kzamanbn@gmail.com>
2025-10-22 09:49:01 +0600

    Update Dokan.php

--- integrations/includes/Dokan/Dokan.php
+++ integrations/includes/Dokan/Dokan.php
@@ -32,8 +32,9 @@ class Dokan implements HookRegistry {
         if ( function_exists( 'dokan_is_seller_dashboard' ) ) {
             Dashboard::instance();
         }
-		Frontend::instance();
+
         Ajax::instance();
+		Frontend::instance();
         Api::instance();
     }
 }
