47114c2cf132fa9d7c0c12a007e0cbf1144d9218
Moudud Ahmed <abdulkader225704@gmail.com>
2026-03-05 11:43:14 +0600

    updated

--- integrations/includes/Dokan/Frontend/Frontend.php
+++ integrations/includes/Dokan/Frontend/Frontend.php
@@ -49,19 +49,21 @@ class Frontend {
         $is_store_name_visible = Helper::find_option_settings( $settings, 'show_quick_cart_dokan_store_names', true );
         $is_store_link_enabled = Helper::find_option_settings( $settings, 'enable_quick_cart_dokan_store_links', true );
         $show_stock_status     = apply_filters( 'spsg_fly_cart_show_stock_status_enabled', false );
+        $has_stock_info        = $show_stock_status && $product->managing_stock() && $product->get_stock_quantity() > 0;
 
-        if ( ! $is_store_name_visible ) {
+        if ( ! $is_store_name_visible && ! $has_stock_info ) {
             return;
         }
-
-        $has_stock_info = $show_stock_status && $product->managing_stock() && $product->get_stock_quantity() > 0;
-        $row_padding    = $has_stock_info ? '55px' : '35px';
         ?>
 
         <style>
             tr:has(td.dokan-vendor-store-info) {
                 position: relative;
-                padding-bottom: <?php echo esc_attr( $row_padding ); ?> !important;
+                padding-bottom: 35px !important;
+            }
+
+            tr:has(td.dokan-vendor-store-info.has-stock-info) {
+                padding-bottom: 55px !important;
             }
 
             td.dokan-vendor-store-info {
@@ -83,7 +85,8 @@ class Frontend {
             }
         </style>
 
-        <td class="dokan-vendor-store-info">
+        <td class="dokan-vendor-store-info<?php echo $has_stock_info ? ' has-stock-info' : ''; ?>">
+            <?php if ( $is_store_name_visible ) : ?>
             <?php
             $vendor    = dokan()->vendor->get( get_post( $product->get_id() )->post_author );
             $shop_name = $vendor->get_shop_name();
@@ -101,6 +104,7 @@ class Frontend {
                     <?php endif; ?>
                 </span>
             </h5>
+            <?php endif; ?>
             <?php if ( $has_stock_info ) : ?>
             <p class="spsg-fly-cart-stock-status" style="margin: 2px 0 0;">
                 <?php echo esc_html( sprintf( /* translators: %d: available stock quantity */ __( 'Available: %d', 'storegrowth-sales-booster' ), $product->get_stock_quantity() ) ); ?>
