3d79b4a0370f4a917a6c6e8d745aec189d55b3e1
Kamruzzaman <kzamanbn@gmail.com>
2026-06-03 10:42:26 +0600

    fix: update STATEGROWTH_VERSION to 2.0.6

--- storegrowth-sales-booster.php
+++ storegrowth-sales-booster.php
@@ -34,7 +34,7 @@ require_once __DIR__ . '/vendor/autoload.php';
  * Define plugin __FILE__
  */
 if ( ! defined( 'STOREGROWTH_VERSION' ) ) {
-	define( 'STOREGROWTH_VERSION', '2.0.2' );
+	define( 'STOREGROWTH_VERSION', '2.0.6' );
 }
 
 /**
